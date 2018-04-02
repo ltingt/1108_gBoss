c9af0460857a59c1707febdaa7c3372b5eb82407
ltingting <l.tingting@51hot.cn>
2018-04-02 18:01:53 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <router-view></router-view>
+    <FootGuide/>
+  </div>
+</template>
+
+<script>
+import FootGuide from './components/FootGuide.vue'
+export default {
+
+  components: {
+    FootGuide
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
